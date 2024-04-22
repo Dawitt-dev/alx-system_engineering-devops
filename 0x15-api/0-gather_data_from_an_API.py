4fb83b49f353f658ca67d856dc0bec16b56c30a2
Dawitt-dev <davoza27@yahoo.com>
2024-04-22 16:45:58 -0700

    shade marmalade

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,11 +7,12 @@ from sys import argv
 
 
 def gather_data(employee_id):
-	"""
-	Retrieves TODO list data for a given employee ID
-	"""	
-    u_url = "https://jsonplaceholder.typicode.com/users/{}".format(employee_id)
-    t_url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(employee_id)
+    """
+    Retrieves TODO list data for a given employee ID
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
+    u_url = "{}/users/{}".format(base_url, employee_id)
+    t_url = "{}/todos?userId={}".format(base_url, employee_id)
 
     user_response = requests.get(u_url)
     todo_response = requests.get(t_url)
@@ -22,7 +23,8 @@ def gather_data(employee_id):
     total_tasks = len(todo_data)
     done_tasks = [task for task in todo_data if task['completed']]
 
-    print("Employee {} is done with tasks({}/{}):".format(user_data['name'], len(done_tasks), total_tasks))
+    print("Employee {} is done with tasks({}/{}):".format(
+        user_data['name'], len(done_tasks), total_tasks))
     for task in done_tasks:
         print("\t {}".format(task['title']))
 
