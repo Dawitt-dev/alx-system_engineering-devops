b3aa8bc4a32cbb09ed8430bcfdd2319714a59c96
Dawitt-dev <davoza27@yahoo.com>
2024-05-07 16:14:44 -0700

    :)

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import requests
 
+
 def number_of_subscribers(subreddit):
     """Returns the number of subscribers for the given subreddit."""
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
