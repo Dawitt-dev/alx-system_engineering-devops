cac16b3a0a063c50a04ddb7382cc07e49b7cc821
Dawitt-dev <davoza27@yahoo.com>
2024-05-08 16:26:34 -0700

    ;(

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,13 @@
+#define an exec resource to fix the issue
+exec { 'fix-wordpress':
+  command     => 'sudo sed -i "s/phpp/php/" /var/www/html/wp-settings.php',
+  path        => ['/usr/bin', '/bin'],
+  returns     => [0, 1],
+  refreshonly => true,
+  notify      => Service['apache2'],
+}
+
+# Ensure Apache service is running
+service { 'apache2':
+  ensure => 'running',
+}
